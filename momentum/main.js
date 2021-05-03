89cbafc0c105a52b6ef35725fa434dd1fa8175c8
liquidGoo <nitor25@gmail.com>
2021-05-03 04:48:42 +0300

    final

--- momentum/main.js
+++ momentum/main.js
@@ -3,7 +3,15 @@ const time = document.getElementById('time'),
     greeting = document.getElementById('greeting')
     nameTag = document.getElementById('name')
     focusTag = document.getElementById('focus'),
-    button = document.getElementById('button');
+    nextBg = document.getElementById('nextBg'),
+    quote = document.getElementById('quote'),
+    author = document.getElementById('author'),
+    nextQuote = document.getElementById('nextQuote'),
+    weatherIcon = document.querySelector('.weather-icon'),
+    temperature = document.getElementById('temperature'),
+    humidity = document.getElementById('humidity'),
+    wind = document.getElementById('wind'),
+    city = document.getElementById('city');
 const bgs = [];
 
 function showTime() {
@@ -96,13 +104,13 @@ function getMonth(n) {
 
 function chooseBgs(hour) {
     for (; hour < 6; hour++) {
-        bgs.push('url(assets/night/' + addZero((Maht.ceil(Math.random() * 20))) + '.jpg)');
+        bgs.push('url(assets/night/' + addZero((Math.ceil(Math.random() * 20))) + '.jpg)');
     }
     for (; hour < 12; hour++) {
-        bgs.push('url(assets/morning/' + addZero((Maht.ceil(Math.random() * 20))) + '.jpg)');
+        bgs.push('url(assets/morning/' + addZero((Math.ceil(Math.random() * 20))) + '.jpg)');
     }
     for (; hour < 18; hour++) {
-        bgs.push('url(assets/day/' + addZero((Maht.ceil(Math.random() * 20))) + '.jpg)');
+        bgs.push('url(assets/day/' + addZero((Math.ceil(Math.random() * 20))) + '.jpg)');
     }
     for (; hour < 24; hour++) {
         bgs.push('url(assets/evening/' + addZero((Math.ceil(Math.random() * 20))) + '.jpg)');
@@ -110,7 +118,7 @@ function chooseBgs(hour) {
 }
 
 function setBgGreet() {
-    button.disabled = true;
+    nextBg.disabled = true;
     let today = new Date(),
         hour = today.getHours();
 
@@ -119,59 +127,51 @@ function setBgGreet() {
     document.body.style.background = bgs.shift();
 
     if (hour < 6) {
-        greeting.textContent = "Good? Night";
-        document.body.style.color = 'black';
-        button.style.filter = 'invert(0%)';
+        greeting.textContent = "Good Night";
+        document.body.style.color = 'white';
     } else if (hour < 12) {
         greeting.textContent = "Good Morning";
         document.body.style.color = 'black';
-        button.style.filter = 'invert(0%)';
     } else if (hour < 18) {
         greeting.textContent = "Good Afternoon";
         document.body.style.color = 'black';
-        button.style.filter = 'invert(0%)';
     } else {
         greeting.textContent = "Good Evening";
         document.body.style.color = 'white';
-        button.style.filter = 'invert(100%)';
     }
 
-    setTimeout(function() {button.disabled = false;}, 1000);
+    setTimeout(function() {nextBg.disabled = false;}, 1000);
     setTimeout(setBgGreet, ((59 - today.getMinutes()) * 60 + (59 - today.getSeconds())) * 1000 + (1000 - today.getMilliseconds()));
 }
 
 function getName() {
-    if (localStorage.getItem('name') === null) {
+    localName = localStorage.getItem('name');
+    if (localName === null || localName === "") {
         nameTag.textContent = '[Enter Name]';
     } else {
-        nameTag.textContent = localStorage.getItem('name');
+        nameTag.textContent = localName;
     }
 }
 
 function getFocus() {
-    if (localStorage.getItem('focus') === null) {
+    localFocus = localStorage.getItem('focus');
+    if (localFocus === null || localFocus === "") {
         focusTag.textContent = '[Enter Focus]';
     } else {
-        focusTag.textContent = localStorage.getItem('focus');
+        focusTag.textContent = localFocus;
     }
 }
 
 function setName(e) {
     if(e.type === 'keypress') {
         if (e.which == 13 || e.keyCode == 13) {
-            if(e.target.innerText === '') {
-                nameTag.textContent = '[Enter Name]';
-            } else {
-                localStorage.setItem('name', e.target.innerText);
-            }
             nameTag.blur();
         }
     } else if (e.type ==='focus' ) {
-        if (e.target.innerText === '[Enter Name]')
-            nameTag.textContent = '';
+        nameTag.textContent = '';
     } else {
         if(e.target.innerText === '') {
-            nameTag.textContent = '[Enter Name]';
+            getName();
         } else {
             localStorage.setItem('name', e.target.innerText);
         }
@@ -180,21 +180,69 @@ function setName(e) {
 function setFocus(e) {
     if(e.type === 'keypress') {
         if (e.which == 13 || e.keyCode == 13) {
-            if(e.target.innerText === '') {
-                focusTag.textContent = '[Enter Focus]';
-            } else {
-                localStorage.setItem('focus', e.target.innerText);
-            }
             focusTag.blur();
         }
     } else if (e.type ==='focus') {
-        if (e.target.innerText === '[Enter Focus]')
-            focusTag.textContent = '';
+        focusTag.textContent = '';
     } else {
         if(e.target.innerText === '') {
-            focusTag.textContent = '[Enter Focus]';
+            getFocus();
         } else {
             localStorage.setItem('focus', e.target.innerText);
+        }
+    }
+}
+
+async function getQuote() {  
+    const url = `https://favqs.com/api/qotd`;
+    const res = await fetch(url);
+    const data = await res.json(); 
+    quote.textContent = data.quote.body;
+    author.textContent = data.quote.author;
+}
+
+async function getWeather() {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&appid=14b072d75b576cfdd2f0c4df6b795c59&units=metric`);
+    const data = await res.json();
+    if (res.ok === false) {
+        weatherIcon.className = 'weather-icon owf';
+        temperature.textContent = data.message;
+        humidity.textContent = '';
+        wind.textContent = '';
+        return false;
+    }
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp}°C`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+    wind.textContent = `Wind: ${data.wind.speed}m/s`
+    return true;
+}
+
+function getCity() {
+    let localCity = localStorage.getItem('city');
+    if (localCity === null || localCity === '') {
+        city.textContent = 'Минск';
+    } else {
+        city.textContent = localCity;
+    }
+}
+
+async function setCity(e) {
+    if(e.type === 'keypress') {
+        if (e.which == 13 || e.keyCode == 13) {
+            city.blur();
+        }
+    } else if (e.type ==='focus' ) {
+        city.textContent = '';
+    } else {
+        if(e.target.innerText === '') {
+            getCity();
+            getWeather();
+        } else {
+            if (await getWeather() == true) {
+                localStorage.setItem('city', e.target.innerText);
+            }
         }
     }
 }
@@ -202,13 +250,24 @@ function setFocus(e) {
 nameTag.addEventListener('keypress', setName);
 nameTag.addEventListener('blur', setName);
 nameTag.addEventListener('focus', setName);
+
 focusTag.addEventListener('keypress', setFocus);
 focusTag.addEventListener('blur', setFocus);
 focusTag.addEventListener('focus', setFocus);
-button.addEventListener('click', setBgGreet);
+
+nextBg.addEventListener('click', setBgGreet);
+
+document.addEventListener('DOMContentLoaded', getQuote);
+nextQuote.addEventListener('click', getQuote);
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
+city.addEventListener('blur', setCity);
+city.addEventListener('focus', setCity);
 
 
 setBgGreet();
 getName();
 getFocus();
 showTime();
+getCity();
